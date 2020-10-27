bc45d9f1fad9122d5eb586228dfe2a6041ae11a6
ArsalanCsquare <aransari@csquareonline.com>
2020-10-27 22:03:09 +0500

    jsx-web-testing

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     presets: ['@babel/preset-react'],
+    plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-syntax-jsx',
+        {
+            loose: true,
+        },
+    ],
 };
